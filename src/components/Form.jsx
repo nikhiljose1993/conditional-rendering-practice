244a2101bc5d8e031ef0067f14338db59edaee38
Nikhil <nikhiljose18@gmail.com>
2022-10-23 22:50:35 +0530

    conditional rendering

---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import Input from "./Input";
+import Button from "./Button";
+
+function Form({ isRegistered }) {
+  return (
+    <form className="form">
+      <Input type={"text"} placeholder={"Email"} />
+      <Input type={"password"} placeholder={"Password"} />
+      {isRegistered ? null : (
+        <Input type={"password"} placeholder={"Confirm Password"} />
+      )}
+      <Button text={isRegistered ? "Login" : "Register"} />
+    </form>
+  );
+}
+
+export default Form;
